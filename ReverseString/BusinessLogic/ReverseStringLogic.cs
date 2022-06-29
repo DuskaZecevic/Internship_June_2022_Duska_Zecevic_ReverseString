25c3c0d7c0f91c179c61ed987a2ba7bc12f1de73
Duska <Duska@DESKTOP-O3AR66J>
2022-06-29 14:37:15 +0200

    Add project files.

---
+++ ReverseString/BusinessLogic/ReverseStringLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReverseString.BusinessLogic
+{
+    public class ReverseStringLogic
+    {
+       public static string ReverseString(char [] arr)
+        {
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                char temp = arr[i];
+                arr[i] = arr[arr.Length - 1 - i];
+                arr[arr.Length - 1 - i] = temp;
+            }
+            return new string(arr);
+        }
+    }
+}
---
+++ ReverseString/Program.cs
@@ -0,0 +1,19 @@
+﻿using ReverseString.BusinessLogic;
+using System;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Write a function that reverses a string.The input string is given as an array of characters s. You must do this by modifying the input
+            //array in-place with O(1) extra memory. Example 1: Input: s = ["h", "e", "l", "l", "o"] - Output: ["o","l","l","e","h"]
+            char[] arr = { 'h', 'e', 'l', 'l', 'o'};
+
+            Console.WriteLine("The reverse string is: " + ReverseStringLogic.ReverseString(arr));
+            
+        }
+    }
+}
